77a8ba7d3f3c7c598283689e2adff1674e4d08d0
kiencuong <daokiencuong04@gmail.com>
2024-11-11 17:34:36 +0700

    Delete all

--- ProjectManagementSystem/src/main/java/com/project/oop/PMS/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.oop.PMS.repository;
-
-import com.project.oop.PMS.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
