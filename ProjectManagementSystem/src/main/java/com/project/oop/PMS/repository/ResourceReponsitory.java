9002ce8e06c0ad320cd82519976c0a4139e44c2c
tuan6100 <tuananhho378@gmail.com>
2024-12-02 21:58:21 +0700

    delete files

--- ProjectManagementSystem/src/main/java/com/project/oop/PMS/repository/ResourceReponsitory.java
+++
@@ -1,7 +0,0 @@
-package com.project.oop.PMS.repository;
-
-import com.project.oop.PMS.entity.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceReponsitory extends JpaRepository<Resource, Integer> {
-}
