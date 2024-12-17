6b7daf14fdf2ce3ef66c055b84326dddef084655
tuan6100 <tuananhho378@gmail.com>
2024-12-17 23:46:08 +0700

    update source

--- ProjectManagementSystem/src/test/java/com/project/oop/PMS/PMSApplicationTests.java
+++ ProjectManagementSystem/src/test/java/com/project/oop/PMS/PMSApplicationTests.java
@@ -1,10 +1,10 @@
-package com.project.oop.hellospringboot;
+package com.project.oop.PMS;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HellospringbootApplicationTests {
+class PMSApplicationTests {
 
 	@Test
 	void contextLoads() {
