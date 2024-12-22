3c79a149dbe1144ef38f4033ce6ff1cb382c71c6
levietducanh99 <ducanhvahung@gmail.com>
2024-12-22 12:19:36 +0700

    rateReport

---
+++ ProjectManagementSystem/src/main/java/com/project/oop/PMS/dto/RateReport.java
@@ -0,0 +1,13 @@
+package com.project.oop.PMS.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RateReport {
+    private Integer id;
+    private String name;
+    private Integer count;
+    private double rate;
+}
