525f787284c263cf4dd9c84182864b0c5a04a3f6
tuan6100 <tuananhho378@gmail.com>
2024-12-27 20:14:05 +0700

    update inheritance

--- ProjectManagementSystem/src/main/java/com/project/oop/PMS/entity/ProjectNotification.java
+++ ProjectManagementSystem/src/main/java/com/project/oop/PMS/entity/ProjectNotification.java
@@ -1,30 +1,25 @@
 package com.project.oop.PMS.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 @Entity
-
 @Table(name = "project_notifications")
 public class ProjectNotification extends Notification {
 
     @Column(name = "project_id", nullable = false)
     private Integer projectId;
 
-    // Constructor
+    public ProjectNotification() {
+        super();
+    }
+
     public ProjectNotification(Integer userId, Integer projectId, String message) {
-        this.setUserId(userId);
-        this.projectId = projectId;
-        this.setMessage(message);
-    }
-
-    public ProjectNotification() {}
-
-    // Getters and Setters
-    public Integer getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Integer projectId) {
+        super(userId, message);
         this.projectId = projectId;
     }
+
 }
