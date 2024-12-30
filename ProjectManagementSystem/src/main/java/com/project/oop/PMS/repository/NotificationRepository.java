fa75b211a7e4da0717707e9737286e7aa6a47d0d
levietducanh99 <ducanhvahung@gmail.com>
2024-12-31 00:11:45 +0700

    dele noti

--- ProjectManagementSystem/src/main/java/com/project/oop/PMS/repository/NotificationRepository.java
+++ ProjectManagementSystem/src/main/java/com/project/oop/PMS/repository/NotificationRepository.java
@@ -3,7 +3,9 @@ package com.project.oop.PMS.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.project.oop.PMS.entity.Notification;
 
@@ -12,4 +14,8 @@ public interface NotificationRepository extends JpaRepository<Notification, Inte
  // Lấy tất cả thông báo theo userId
     @Query("SELECT n FROM Notification n WHERE n.userId = ?1")
     List<Notification> findAllByUserId(Integer userId);
+    // Xóa thông báo theo ID nếu bạn muốn query tuỳ chỉnh
+    @Modifying
+    @Query("DELETE FROM Notification n WHERE n.id = :id")
+    void deleteNotificationById(@Param("id") Integer id);
 }
