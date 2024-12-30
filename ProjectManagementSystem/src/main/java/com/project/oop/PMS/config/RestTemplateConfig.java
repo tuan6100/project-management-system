6a0bad07a70c6b6dab324e0e1121a674fb101b2b
tuan6100 <tuananhho378@gmail.com>
2024-12-30 11:39:51 +0700

    upload file

--- ProjectManagementSystem/src/main/java/com/project/oop/PMS/config/RestTemplateConfig.java
+++ ProjectManagementSystem/src/main/java/com/project/oop/PMS/config/RestTemplateConfig.java
@@ -11,29 +11,34 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
-import java.io.File;
+import java.io.FileInputStream;
 import java.security.KeyStore;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Value("${server.ssl.trust-store}")
-    private KeyStore trustStore;
+    private String trustStorePath;
 
     @Value("${server.ssl.trust-store-password}")
     private String trustStorePassword;
 
+
     @Bean
     public RestTemplate restTemplate() throws Exception {
-        SSLContext sslContext = new SSLContextBuilder()
-                .loadTrustMaterial((File) trustStore.getKey(String.valueOf(trustStore), trustStorePassword.toCharArray()), trustStorePassword.toCharArray())
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        try (FileInputStream trustStoreStream = new FileInputStream(trustStorePath)) {
+            trustStore.load(trustStoreStream, trustStorePassword.toCharArray());
+        }
+        SSLContext sslContext = SSLContextBuilder.create()
+                .loadTrustMaterial(trustStore, null)
                 .build();
-        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
         HttpClient httpClient = (HttpClient) HttpClients.custom()
-                .setSSLSocketFactory(socketFactory)
+                .setSSLSocketFactory(new SSLConnectionSocketFactory(sslContext))
                 .build();
         HttpComponentsClientHttpRequestFactory factory =
                 new HttpComponentsClientHttpRequestFactory(httpClient);
         return new RestTemplate(factory);
     }
+
 }
